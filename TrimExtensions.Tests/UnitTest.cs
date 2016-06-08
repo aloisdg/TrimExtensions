635b235df14be1c321c544bac821df4d64a35e1b
aloisdg <aloisdegouvello@live.fr>
2016-06-08 04:10:37 +0200

    feat(tests): Enforce TDD by writing tests first

---
+++ TrimExtensions.Tests/UnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace TrimExtensions.Tests {
+    [TestFixture]
+    public class UnitTest {
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab")]
+        public string TestTrimAfterFirstOf(string source, string delimiter) {
+            var actual = source.TrimAfterFirstOf (delimiter);
+            return actual;
+        }
+
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab;cd")]
+        public string TestTrimAfterLastOf(string source, string delimiter) {
+            var actual = source.TrimAfterLastOf (delimiter);
+            return actual;
+        }
+
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = "cd;ef")]
+        public string TestTrimBeforeFirstOf(string source, string delimiter) {
+            var actual = source.TrimBeforeFirstOf (delimiter);
+            return actual;
+        }
+
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = "ef")]
+        public string TestTrimBeforeLastOf(string source, string delimiter) {
+            var actual = source.TrimBeforeLastOf (delimiter);
+            return actual;
+        }
+    }
+}
