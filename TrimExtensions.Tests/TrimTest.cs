7286cf315f4e148e4edb504e5a413814d40fa512
aloisdg <aloisdegouvello@live.fr>
2016-06-08 05:10:19 +0200

    feat(tests): Add missing parameters

--- TrimExtensions.Tests/TrimTest.cs
+++ TrimExtensions.Tests/TrimTest.cs
@@ -7,28 +7,28 @@ namespace TrimExtensions.Tests {
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab")]
         [TestCase ("ab;cd;ef", ";", true, ExpectedResult = "ab;")]
         public string TestTrimAfterFirstOf(string source, string delimiter, bool hasDelimiter = false) {
-            var actual = source.TrimAfterFirstOf (delimiter);
+            var actual = source.TrimAfterFirstOf (delimiter, hasDelimiter);
             return actual;
         }
 
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab;cd")]
-        [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab;cd;")]
-        public string TestTrimAfterLastOf(string source, string delimiter) {
-            var actual = source.TrimAfterLastOf (delimiter);
+        [TestCase ("ab;cd;ef", ";", true, ExpectedResult = "ab;cd;")]
+        public string TestTrimAfterLastOf(string source, string delimiter, bool hasDelimiter = false) {
+            var actual = source.TrimAfterLastOf (delimiter, hasDelimiter);
             return actual;
         }
 
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "cd;ef")]
-        [TestCase ("ab;cd;ef", ";", ExpectedResult = ";cd;ef")]
-        public string TestTrimBeforeFirstOf(string source, string delimiter) {
-            var actual = source.TrimBeforeFirstOf (delimiter);
+        [TestCase ("ab;cd;ef", ";", true, ExpectedResult = ";cd;ef")]
+        public string TestTrimBeforeFirstOf(string source, string delimiter, bool hasDelimiter = false) {
+            var actual = source.TrimBeforeFirstOf (delimiter, hasDelimiter);
             return actual;
         }
 
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "ef")]
-        [TestCase ("ab;cd;ef", ";", ExpectedResult = ";ef")]
-        public string TestTrimBeforeLastOf(string source, string delimiter) {
-            var actual = source.TrimBeforeLastOf (delimiter);
+        [TestCase ("ab;cd;ef", ";", true, ExpectedResult = ";ef")]
+        public string TestTrimBeforeLastOf(string source, string delimiter, bool hasDelimiter = false) {
+            var actual = source.TrimBeforeLastOf (delimiter, hasDelimiter);
             return actual;
         }
     }
