acf971909df0ca95b33882c7124b5971a4567379
aloisdg <aloisdegouvello@live.fr>
2016-06-08 05:10:55 +0200

    feat(lib): Allow to keep delimiter

--- TrimExtensions/TrimEx.cs
+++ TrimExtensions/TrimEx.cs
@@ -6,29 +6,33 @@ using System.Text;
 namespace TrimExtensions {
     public static class TrimEx {
         // ab;cd;ef, ; => ab
-        public static string TrimAfterFirstOf(this string source, string delimiter) {
-            var end = source.IndexOf (delimiter) + delimiter.Length - 1;
+        public static string TrimAfterFirstOf(this string source, string delimiter, bool hasDelimiter = false) {
+            var delimiterSize = hasDelimiter ? delimiter.Length : 0;
+            var end = source.IndexOf (delimiter) + delimiterSize;
             var result = source.Remove (end);
             return result;
         }
 
         // ab;cd;ef, ; => ab;cd
-        public static string TrimAfterLastOf(this string source, string delimiter) {
-            var end = source.LastIndexOf (delimiter) + delimiter.Length - 1;
+        public static string TrimAfterLastOf(this string source, string delimiter, bool hasDelimiter = false) {
+            var delimiterSize = hasDelimiter ? delimiter.Length : 0;
+            var end = source.LastIndexOf (delimiter) + delimiterSize;
             var result = source.Remove (end);
             return result;
         }
 
         // ab;cd;ef, ; => cd;ef
-        public static string TrimBeforeFirstOf(this string source, string delimiter) {
-            var start = source.IndexOf (delimiter);
+        public static string TrimBeforeFirstOf(this string source, string delimiter, bool hasDelimiter = false) {
+            var delimiterSize = !hasDelimiter ? delimiter.Length : 0;
+            var start = source.IndexOf (delimiter) + delimiterSize;
             var result = source.Substring (start);
             return result;
         }
 
         // ef, ; => ab;cd
-        public static string TrimBeforeLastOf(this string source, string delimiter) {
-            var start = source.LastIndexOf (delimiter);
+        public static string TrimBeforeLastOf(this string source, string delimiter, bool hasDelimiter = false) {
+            var delimiterSize = !hasDelimiter ? delimiter.Length : 0;
+            var start = source.LastIndexOf (delimiter) + delimiterSize;
             var result = source.Substring (start);
             return result;
         }
