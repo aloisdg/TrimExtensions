f09f157c942664888cca4db5f623fbdf97a45a1a
aloisdg <aloisdegouvello@live.fr>
2016-06-08 03:45:14 +0200

    feat(lib): Write functions prototype

--- TrimExtensions/TrimEx.cs
+++ TrimExtensions/TrimEx.cs
@@ -3,9 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace TrimExtensions
-{
-    public class TrimEx
-    {
+namespace TrimExtensions {
+    public static class TrimEx {
+        // ab;cd;ef, ; => ab
+        private static string TrimAfterFirstOf(string s, string delimiter) {
+            return s;
+        }
+
+        // ab;cd;ef, ; => ab;cd
+        private static string TrimAfterLastOf(this string s, string delimiter) {
+            return s;
+        }
+
+        // ab;cd;ef, ; => cd;ef
+        private static string TrimBeforeFirstOf(this string s, string delimiter) {
+            return s;
+        }
+
+        // ef, ; => ab;cd
+        private static string TrimBeforeLastOf(this string s, string delimiter) {
+            return s;
+        }
     }
 }
