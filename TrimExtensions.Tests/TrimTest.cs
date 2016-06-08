0901be3b61befc4f755e8b8223d7157df554a040
aloisdg <aloisdegouvello@live.fr>
2016-06-08 04:49:52 +0200

    feat(tests): Allow to keep delimiter

--- TrimExtensions.Tests/TrimTest.cs
+++ TrimExtensions.Tests/TrimTest.cs
@@ -5,24 +5,28 @@ namespace TrimExtensions.Tests {
     [TestFixture]
     public class TrimTest {
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab")]
-        public string TestTrimAfterFirstOf(string source, string delimiter) {
+        [TestCase ("ab;cd;ef", ";", true, ExpectedResult = "ab;")]
+        public string TestTrimAfterFirstOf(string source, string delimiter, bool hasDelimiter = false) {
             var actual = source.TrimAfterFirstOf (delimiter);
             return actual;
         }
 
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab;cd")]
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = "ab;cd;")]
         public string TestTrimAfterLastOf(string source, string delimiter) {
             var actual = source.TrimAfterLastOf (delimiter);
             return actual;
         }
 
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "cd;ef")]
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = ";cd;ef")]
         public string TestTrimBeforeFirstOf(string source, string delimiter) {
             var actual = source.TrimBeforeFirstOf (delimiter);
             return actual;
         }
 
         [TestCase ("ab;cd;ef", ";", ExpectedResult = "ef")]
+        [TestCase ("ab;cd;ef", ";", ExpectedResult = ";ef")]
         public string TestTrimBeforeLastOf(string source, string delimiter) {
             var actual = source.TrimBeforeLastOf (delimiter);
             return actual;
