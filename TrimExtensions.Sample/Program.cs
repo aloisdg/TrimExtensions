69d44acee3fa75d746fe2c2048b30d84d9f4572a
aloisdg <aloisdegouvello@live.fr>
2016-06-08 04:48:47 +0200

    feat(sample): Show a simple use case of the lib

---
+++ TrimExtensions.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrimExtensions.Sample {
+    class Program {
+        static void Main(string[] args) {
+            const string source = "ab;cd;ef";
+
+            Console.WriteLine (source.TrimAfterLastOf (";"));
+            Console.WriteLine (source.TrimAfterFirstOf (";"));
+            Console.WriteLine (source.TrimBeforeLastOf (";"));
+            Console.WriteLine (source.TrimBeforeFirstOf (";"));
+            Console.ReadLine();
+        }
+    }
+}
